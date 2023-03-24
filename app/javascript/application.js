460dd0c5a6bcd58335acdcd1af08d5c86a756d26
Atekelt Afework <atekeltafework55@gmail.com>
2023-03-24 14:05:40 -0700

    Boiler PLate project for altra

---
+++ app/javascript/application.js
@@ -0,0 +1,19 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "custom/main"
+
+// import "custom/aos"
+import "custom/bootstrap.bundle"
+import "custom/bootstrap.bundle.min"
+// import "custom/bootstrap.esm"
+// import "custom/bootstrap.esm.min"
+import "custom/bootstrap"
+import "custom/bootstrap.min"
+import "custom/glightbox"
+import "custom/glightbox.min"
+import "custom/isotope.pkgd"
+import "custom/isotope.pkgd.min"
+import "custom/purecounter_vanilla"
+
+import 'swiper/swiper-bundle.min.css'
---
+++ config/importmap.rb
@@ -0,0 +1,13 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/custom", under: "custom"
+pin "swiper/element/bundle", to: "https://ga.jspm.io/npm:swiper@9.1.1/element/swiper-element-bundle.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@8.4.7/swiper-bundle.esm.js"
+pin "stimulus-carousel", to: "https://ga.jspm.io/npm:stimulus-carousel@5.0.1/dist/stimulus-carousel.mjs"
+pin "dom7", to: "https://ga.jspm.io/npm:dom7@4.0.6/dom7.esm.js"
